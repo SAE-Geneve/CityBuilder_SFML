cdbc5fe4141dcac35e20a191e6b2b48a7f3db610
Elias Farhan <elias.farhan@gmail.com>
2025-05-21 14:28:29 +0200

    Adding constexpr addition and testing accordingly

--- core/include/maths/vec2f.h
+++ core/include/maths/vec2f.h
@@ -2,13 +2,14 @@
 #define VEC2F_H
 
 namespace core {
-
 class Vec2f {
 public:
   float x = 0.0f, y = 0.0f;
 
+  constexpr Vec2f operator+(const Vec2f & v2) const noexcept {
+    return {x+v2.x, y+v2.y};
+  }
+
 };
-
 }
-
 #endif //VEC2F_H
--- core/test/test_vec2f.cc
+++ core/test/test_vec2f.cc
@@ -6,3 +6,20 @@ TEST(Vec2f, Empty) {
   EXPECT_FLOAT_EQ(0, v.x);
   EXPECT_FLOAT_EQ(0, v.y);
 }
+
+TEST(Vec2f, Add) {
+  constexpr float x1 = 1, x2 = 2;
+  constexpr float y1 = 3, y2 = -4;
+  constexpr core::Vec2f v1{x1, y1};
+  constexpr core::Vec2f v2{x2, y2};
+
+  constexpr auto result = v1+v2;
+
+  static_assert(result.x == x1 + x2);
+  static_assert(result.y == y1 + y2);
+
+  EXPECT_FLOAT_EQ(result.x, x1+x2);
+  EXPECT_FLOAT_EQ(result.y, y1+y2);
+
+
+}
