b90aad13a9ac9206a152ba2bfd2a59bd8911e44b
Elias Farhan <elias.farhan@gmail.com>
2025-06-04 17:08:27 +0200

    Adding an experimental asset manager with its test

--- core/include/assets/asset_manager.h
+++ core/include/assets/asset_manager.h
@@ -1,8 +1,10 @@
 ﻿#ifndef ASSET_MANAGER_H
 #define ASSET_MANAGER_H
 
+#include <span>
 #include <string>
 #include <vector>
+#include <array>
 
 /* TODO: AssetManager
 1 - can handle every type of media : sf::Texture, sf::soundBuffer, sf::Font, etc..
@@ -68,6 +70,43 @@ const TAsset &AssetManager<TAsset>::Get(TextureIndex textIndex){
     }
 
     return assets_[index];
+}
+
+namespace core::experimental {
+
+template<size_t N>
+struct StringLiteral {
+  constexpr StringLiteral(const char (&arg)[N]) {
+    std::copy_n(arg, N, str);
+  }
+  char str[N];
+};
+
+template<typename TAsset, typename TAssetType, StringLiteral folderPath>
+requires std::is_enum_v<TAssetType> && requires
+{
+  {static_cast<size_t>(TAssetType::kLength)};
+}
+class AssetManager {
+  public:
+
+  void Load(std::span<const std::string_view> assetsPaths) {
+    for (size_t i = 0; i < assets_.size(); ++i) {
+      assets_[i] = TAsset(std::format("{}/{}", folderPath.str, assetsPaths[i]));
+    }
+  }
+
+  const TAsset &Get(TAssetType asset_index) {
+    const auto index = static_cast<int64_t>(asset_index);
+    if (index < 0 || index >= std::ssize(assets_)) {
+      throw std::out_of_range("Invalid index");
+    }
+    return assets_[index];
+  }
+private:
+  std::array<TAsset, static_cast<size_t>(TAssetType::kLength)> assets_{};
+};
+
 }
 
 
