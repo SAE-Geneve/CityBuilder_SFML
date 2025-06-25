b701220618c59e18953240bdc211b1e219478a76
StudioAlbert <account@studio-albert.com>
2025-06-25 23:46:05 +0200

    Button factory

--- game/src/game.cc
+++ game/src/game.cc
@@ -4,108 +4,88 @@
 #include "ai/npc_manager.h"
 #include "graphics/tilemap.h"
 #include "ui/button.h"
+#include "ui/button_factory.h"
 #include "ui/clickable.h"
 
 namespace game {
-namespace {
+    namespace {
+        sf::Clock clock;
+        sf::RenderWindow window_;
 
-sf::Clock clock;
-sf::RenderWindow window_;
+        auto tilemap_ptr_ = std::make_unique<TileMap>();
+        api::ai::NpcManager npc_manager_;
 
-auto tilemap_ptr_ = std::make_unique<TileMap>();
-api::ai::NpcManager npc_manager_;
+        // UI Elements
+        std::unique_ptr<api::ui::Button> btnBlue;
+        std::unique_ptr<api::ui::Button> btnRed;
+        std::unique_ptr<api::ui::Button> btnGreen;
 
-// UI Elements
-std::unique_ptr<api::ui::Button> btnBlue;
-std::unique_ptr<api::ui::Button> btnRed;
-std::unique_ptr<api::ui::Button> btnGreen;
+        api::ai::NpcType npc_adding_type = api::ai::NpcType::kNone;
 
-sf::Font font;
+        api::ui::ButtonFactory btn_factory;
 
-api::ai::NpcType npc_adding_type = api::ai::NpcType::kNone;
+        void Setup(){
+            // Create the main window
+            window_.create(sf::VideoMode({1280, 1080}), "SFML window");
 
-enum class UISprite { kEmpty, kBgBtn, kHoverBtn, kLength };
-std::array<std::string_view, static_cast<size_t>(UISprite::kLength)> UIfiles_ =
-    {"empty.png", "button_grey.png", "button_brown.png"};
-AssetManager<sf::Texture, UISprite, "_assets/sprites"> UItextures_;
+            tilemap_ptr_->Setup();
+            tilemap_ptr_->OnReleasedLeft = []() {
+                std::cout << "Clicked tilemap" << "\n";
+                npc_manager_.Add(npc_adding_type,
+                                 TileMap::TilePos(sf::Mouse::getPosition(window_)),
+                                 tilemap_ptr_.get());
+                npc_adding_type = api::ai::NpcType::kNone;
+            };
 
-void Setup() {
-  // Create the main window
-  window_.create(sf::VideoMode({1280, 1080}), "SFML window");
 
-  tilemap_ptr_->Setup();
-  tilemap_ptr_->OnReleasedLeft = []() {
+            btnBlue = btn_factory.CreateButton(sf::Vector2f(100.f, window_.getSize().y - 100.f), "Blue");
+            btnBlue->OnReleasedLeft = []() { npc_adding_type = api::ai::NpcType::kBlue; };
 
-    std::cout << "Clicked tilemap" << "\n";
-    npc_manager_.Add(npc_adding_type,
-                     TileMap::TilePos(sf::Mouse::getPosition(window_)),
-                     tilemap_ptr_.get());
-    npc_adding_type = api::ai::NpcType::kNone;
+            btnRed = btn_factory.CreateButton(sf::Vector2f(200.f, window_.getSize().y - 100.f), "Red");
+            btnRed->OnReleasedLeft = []() { npc_adding_type = api::ai::NpcType::kRed; };
 
-  };
+            btnGreen = btn_factory.CreateButton(sf::Vector2f(300.f, window_.getSize().y - 100.f), "Green");
+            btnGreen->OnReleasedLeft = []() {npc_adding_type = api::ai::NpcType::kGreen; };
 
-  UItextures_.Load(UIfiles_);
-  font.openFromFile("_assets/fonts/ANTQUAB.TTF");
+        }
+    } // namespace
 
-  btnBlue = std::make_unique<api::ui::Button>(
-      sf::Vector2f(100.f, window_.getSize().y - 100.f), "Blue",
-      UItextures_.Get(UISprite::kBgBtn), UItextures_.Get(UISprite::kHoverBtn),
-      font);
-  btnBlue->OnReleasedLeft = []() { npc_adding_type = api::ai::NpcType::kBlue; };
+    void Loop(){
+        Setup();
 
-  btnRed = std::make_unique<api::ui::Button>(
-      sf::Vector2f(200.f, window_.getSize().y - 100.f), "Red",
-      UItextures_.Get(UISprite::kBgBtn), UItextures_.Get(UISprite::kHoverBtn),
-      font);
-  btnRed->OnReleasedLeft = []() { npc_adding_type = api::ai::NpcType::kRed; };
+        // Start the game loop
+        while (window_.isOpen()) {
+            auto dt = clock.restart().asSeconds();
 
-  btnGreen = std::make_unique<api::ui::Button>(
-      sf::Vector2f(300.f, window_.getSize().y - 100.f), "Green",
-      UItextures_.Get(UISprite::kBgBtn), UItextures_.Get(UISprite::kHoverBtn),
-      font);
-  btnGreen->OnReleasedLeft = []() {
-    npc_adding_type = api::ai::NpcType::kGreen;
-  };
-}
-}  // namespace
+            // Process events = Input frame
+            while (const std::optional event = window_.pollEvent()) {
+                // Close window: exit
+                if (event->is<sf::Event::Closed>()) {
+                    window_.close();
+                }
 
-void Loop() {
-  Setup();
+                bool buttonsWasClicked = false;
+                btnBlue->HandleEvent(event, buttonsWasClicked);
+                btnRed->HandleEvent(event, buttonsWasClicked);
+                btnGreen->HandleEvent(event, buttonsWasClicked);
 
-  // Start the game loop
-  while (window_.isOpen()) {
-    auto dt = clock.restart().asSeconds();
+                tilemap_ptr_->HandleEvent(event, buttonsWasClicked);
+            }
 
-    // Process events = Input frame
-    while (const std::optional event = window_.pollEvent()) {
-      // Close window: exit
-      if (event->is<sf::Event::Closed>()) {
-        window_.close();
-      }
+            // GamePlay, physic frame
+            npc_manager_.Update(dt);
 
-      bool buttonsWasClicked = false;
-      btnBlue->HandleEvent(event, buttonsWasClicked);
-      btnRed->HandleEvent(event, buttonsWasClicked);
-      btnGreen->HandleEvent(event, buttonsWasClicked);
+            // Graphic frame
+            window_.clear();
 
-      tilemap_ptr_->HandleEvent(event, buttonsWasClicked);
+            tilemap_ptr_->Draw(window_);
+            npc_manager_.Draw(window_);
 
+            btnBlue->Draw(window_);
+            btnRed->Draw(window_);
+            btnGreen->Draw(window_);
+
+            window_.display();
+        }
     }
-
-    // GamePlay, physic frame
-    npc_manager_.Update(dt);
-
-    // Graphic frame
-    window_.clear();
-
-    tilemap_ptr_->Draw(window_);
-    npc_manager_.Draw(window_);
-
-    btnBlue->Draw(window_);
-    btnRed->Draw(window_);
-    btnGreen->Draw(window_);
-
-    window_.display();
-  }
-}
-}  // namespace game
+} // namespace game
